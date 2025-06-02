6777f2f3e80c6eb92c49e023d7b0ee31d495657b
Chintya-aa <tyachintyachin@gmail.com>
2025-06-02 17:27:41 +0700

    first commit

---
+++ polymorphy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+    //virtual void pesan() = 0;
+    virutal void pesan()
+    {
+        cout << "Pesan dari seseorang" << end
+    }
+}
